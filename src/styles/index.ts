2423933724626b513dccbb4de6868570cfb962c0
APdlb <alepdlb@gmail.com>
2021-11-20 18:07:21 +0100

    chore: Add global styles.

---
+++ src/styles/index.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+
+  #root {
+    min-height: 100vh,
+    font-size: 16px;
+  }
+`;
+
+export default GlobalStyle;
